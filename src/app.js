d45cc4aae41b581521d85a9a5d491e0647234316
Iñigo Menezo López <118929768+inigomenezolopez@users.noreply.github.com>
2024-10-01 03:19:03 +0000

    añadido codigo con excusas random

--- src/app.js
+++ src/app.js
@@ -2,10 +2,74 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document.addEventListener("DOMContentLoaded", function() {
+  // array con las palabras
+  let inicio = "un ";
+  let adjetivos = [
+    "bicéfalo ",
+    "radiactivo ",
+    "enojado ",
+    "solitario ",
+    "loco ",
+    "brillante ",
+    "apestoso ",
+    "lento ",
+    "viejo "
+  ];
+  let sustantivos = [
+    "corredor ",
+    "mapache ",
+    "perro ",
+    "vendedor ",
+    "conductor ",
+    "comediante ",
+    "piña "
+  ];
+  let acciones = [
+    "tomó mi ",
+    "lanzó mi ",
+    "gritó a mi ",
+    "pateó mi ",
+    "robó mi ",
+    "quemó mi ",
+    "mordió mi ",
+    "golpeó mi "
+  ];
+  let posesiones = [
+    "dedo ",
+    "coche ",
+    "reloj ",
+    "zapato ",
+    "billetera ",
+    "camisa ",
+    "llaves ",
+    "ordenador ",
+    "teléfono ",
+    "bocadillo "
+  ];
+  let lugares = [
+    "en la calle",
+    "en mi casa",
+    "en mi garaje",
+    "frente a la oficina",
+    "cerca del centro comercial",
+    "cerca del baño",
+    "en la estación de autobuses"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // declarando variables aleatorias
+  let aleatorio1 = Math.floor(Math.random() * adjetivos.length);
+  let aleatorio2 = Math.floor(Math.random() * sustantivos.length);
+  let aleatorio3 = Math.floor(Math.random() * acciones.length);
+  let aleatorio4 = Math.floor(Math.random() * posesiones.length);
+  let aleatorio5 = Math.floor(Math.random() * lugares.length);
+
+  // creando una frase (la excusa)
+  document.querySelector("#excusa").innerHTML =
+    inicio +
+    adjetivos[aleatorio1] +
+    sustantivos[aleatorio2] +
+    acciones[aleatorio3] +
+    posesiones[aleatorio4] +
+    lugares[aleatorio5];
+});
